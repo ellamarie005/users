0347a49856c651d2769f14a74302febc9b96548d
ellamarie005 <ellatolentino05@gmail.com>
2018-10-08 20:15:26 -0700

    initial project: mongo node expg express with stephen grider

---
+++ test/test_helper.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+// on my present machine find this database
+mongoose.connect('mongodb://localhost/users_test');
+mongoose.connection
+  .once('open', () => console.log('Good to go!'))
+  .on('error', (error) => {
+    console.warn('Warning', error);
+  });
